13435c94b583984d911d4ecc1a73db2e8d8768d7
miha <miha@sowalabs.com>
2013-03-06 15:17:39 +0100

    Synced bootstrap navigation controls with highstock

--- Demos/SentimentIndex/js/code.js
+++ Demos/SentimentIndex/js/code.js
@@ -1,32 +1,77 @@
-$('#datetimepickerFrom').datetimepicker({
+var chart;
+var MIN_DATE 
+	= Date.UTC(2012, 0, 1); // Jan 1, 2012
+var MAX_DATE 
+	= Date.UTC(2012, 11, 31, 12, 0); // Dec 31, 2012
+var DAY_SPAN
+	= 86400000;
+
+// initialize date pickers
+$("#datepicker-from,#datepicker-to").datetimepicker({
 	pickTime: false
+}).on("changeDate", function(e) {
+	var dateStart = $("#datepicker-from").data("datetimepicker").getDate().getTime();
+	var dateEnd = $("#datepicker-to").data("datetimepicker").getDate().getTime();
+	if (dateEnd < dateStart) { dateEnd = dateStart; }
+	dateStart = Math.max(MIN_DATE, Math.min(dateStart, MAX_DATE));
+	dateEnd = Math.max(MIN_DATE, Math.min(dateEnd, MAX_DATE));
+	chart.xAxis[0].setExtremes(dateStart, dateEnd);
 });
 
-$('#datetimepickerTo').datetimepicker({
-	pickTime: false
+// set initial time span
+$("#datepicker-from").data("datetimepicker").setDate(MIN_DATE);
+$("#datepicker-to").data("datetimepicker").setDate(MAX_DATE);
+
+// assign button handlers
+$(".btn").click(function() {
+	var action = $(this)[0].textContent;
+	var span;
+	var dateEnd = chart.xAxis[0].getExtremes().max;
+	if (action == "All") { chart.xAxis[0].setExtremes(MIN_DATE, MAX_DATE); return; }
+	else if (action == "1y") { span = 365 * DAY_SPAN; }
+	else if (action == "1m") { span = 30 * DAY_SPAN; }
+	else if (action == "3m") { span = 90 * DAY_SPAN; }
+	else if (action == "6m") { span = 180 * DAY_SPAN; }
+	var dateStart = dateEnd - span;
+	if (dateStart < MIN_DATE) { dateEnd += (MIN_DATE - dateStart); dateStart += (MIN_DATE - dateStart); }
+	chart.xAxis[0].setExtremes(dateStart, dateEnd);
 });
+
+function filter(data) {
+	var newData = [];
+	for (var i in data) {
+		if (data[i][0] >= MIN_DATE && data[i][0] <= MAX_DATE) {
+			var d = new Date(data[i][0]);
+			d = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()) + 43200000; // data point appear at midday
+			newData.push([d, data[i][1]]); 
+		}
+	}
+	return newData;
+}
 
 function load(name) {
-	$.getJSON('http://first-vm4.ijs.si/first_occurrence/data/?label=' + name + '&callback=?&w=1', function(data) {
-		$.getJSON('http://first-vm4.ijs.si/first_sentiment/data/?scope=document&aggregation=sum&label=' + name + '&callback=?&w=1', function(sentiment) {
-			new Highcharts.StockChart({
+	$.getJSON("http://first-vm4.ijs.si/first_occurrence/data/?label=" + name + "&callback=?&w=1", function(volume) {
+		$.getJSON("http://first-vm4.ijs.si/first_sentiment/data/?scope=document&aggregation=sum&label=" + name + "&callback=?&w=1", function(sentiment) {
+			chart = new Highcharts.StockChart({
 				credits: { 
 					enabled: false 
 				},
 				chart: {
-					renderTo: "chart_container",
+					renderTo: "chart-container",
 					zoomType: "x",
 				},
 				rangeSelector: { 
-					selected: 1 
+					enabled: false
 				},
 				xAxis: {
 					events: {
 						setExtremes: function(event) {
-							console.log(
-								Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', event.min),
-								Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', event.max)
-							);
+							$("#datepicker-from").data("datetimepicker").setDate(event.min);
+							$("#datepicker-to").data("datetimepicker").setDate(event.max);
+							if (Math.abs(event.max - event.min - chart.span) >= 2 * DAY_SPAN) {
+								chart.span = event.max - event.min;
+								$(".btn-group button").removeClass("active");
+							}
 						}
 					}
 				},
@@ -39,7 +84,7 @@ function load(name) {
 							align: "right",
 							x: -5,
 							formatter: function() {
-								return (this.value > 0 ? '+' : '') + this.value;
+								return (this.value > 0 ? "+" : "") + this.value;
 							}
 						},
 						plotLines: [{
@@ -58,7 +103,7 @@ function load(name) {
 							align: "right",
 							x: -5,
 							formatter: function () {
-								return (this.value > 0 ? '+' : '') + this.value;
+								return (this.value > 0 ? "+" : "") + this.value;
 							}
 						},
 						plotLines:[{
@@ -73,14 +118,15 @@ function load(name) {
 				}],
 				series: [{
 					name: "Volume", 
-					data: data
+					data: filter(volume)
 				},
 				{
 					name: "Sentiment", 
 					yAxis: 1,
-					data: sentiment
+					data: filter(sentiment)
 				}]
 			});
+			chart.span = MAX_DATE - MIN_DATE;
 		});
 	});
 }
